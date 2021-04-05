902fbf9e097829f200b9cc2514474d62b7c29b38
ElLorans <lorenzo.cerreta@libero.it>
2021-04-05 15:32:48 +0200

    Changed folder name

---
+++ Best Mtg Deck/bestdeck.py
@@ -0,0 +1,289 @@
+"""
+Library to get relevant info from collection and database of MTG Tier decks.
+"""
+from typing import List, Union, Any, Dict
+
+from database import Standard, Brawl, Historic, Pioneer, Modern, Legacy, LegacyBudgetToTier, Pauper, Vintage, Cube, \
+    Historic_Brawl, Commander, Commander_1v1, \
+    Standard_Sideboards, Historic_Sideboards, Pioneer_Sideboards, Modern_Sideboards, \
+    Legacy_Sideboards, LegacyBudgetToTier_Sideboards, Pauper_Sideboards, Vintage_Sideboards, Commander_1v1_Sideboards
+from rarity import rarity
+
+
+BASIC_LANDS = ('Forest', 'Swamp', 'Mountain', 'Plains', 'Island')
+
+
+def merge(dict_1: Union[Dict[str, int], None], dict_2: Union[Dict[str, int], None]) -> dict:
+    """
+    Merge 2 dicts of ints: keys from both dicts, values are the sum of values (one dict can be None).
+    :dict_1: {"a": 1, "b": 2}
+    :dict_2: {"a": 3, "c": 3} or None
+    :return: {"a": 4, "b": 2, "c": 3} or {"a": 1, "b": 2}
+    """
+    if dict_2 is None:
+        return dict_1
+    elif dict_1 is None:
+        return dict_2
+    summa = dict()
+
+    for key in dict_1.keys() | dict_2.keys():  # union of all keys
+        summa[key] = dict_1.get(key, 0) + dict_2.get(key, 0)  # sum quantities or 0 if key not found
+
+    return summa
+
+
+def get_sideboard_dict(format_name: str) -> Union[Dict[str, dict], None]:
+    """
+    Return sideboard dict of dicts associated to format_name (format_name_Sideboards).
+    :param format_name: string (e.g.: 'Modern' or 'Legacy')
+    :return: dict (format_name_Sideboards) or None
+    """
+    str_to_side = {"Legacy": Legacy_Sideboards, "Standard": Standard_Sideboards, "Modern": Modern_Sideboards,
+                   "Pauper": Pauper_Sideboards, "Pioneer": Pioneer_Sideboards,
+                   "Legacy Budget To": LegacyBudgetToTier_Sideboards, "Historic": Historic_Sideboards,
+                   "Vintage": Vintage_Sideboards, "Commander 1vs1": Commander_1v1_Sideboards}
+    return str_to_side.get(format_name)
+
+
+class Deck:
+    def __init__(self, name: str, mainboard: Dict[str, int], format_name: str, format_dict: dict, coll_dict: dict,
+                 card_prices: dict, sideboard=None):
+        """
+        Do all calculations on MTG deck given coll_dict.
+        :param name: str (e.g.: 'Tron')
+        :param mainboard: Dict[str, int]
+        :param format_name: string (e.g.: 'Modern')
+        :param format_dict: dict (e.g.: Modern)
+        :param coll_dict:
+        :param card_prices: dict of prices (can be USD or EUR)
+        :param sideboard: Dict[str, int]
+        """
+        self.name = name
+        self.mainboard = mainboard  # dict
+        if sideboard is None:
+            if get_sideboard_dict(format_name) is not None:
+                self.sideboard = get_sideboard_dict(format_name)[name]
+            else:
+                self.sideboard = None
+        if format_name in ("Standard", "Brawl", "Historic", "Historic Brawl"):
+            self.arena = True
+            self.wc = {'Mythic': 0, 'Rare': 0, 'Uncommon': 0, 'Common': 0}  # wc YOU NEED
+        else:
+            self.arena = False
+            self.wc = None
+
+        self.format_name = format_name
+        self.formato = format_dict
+        self.collection = coll_dict
+        self.total = merge(self.mainboard, self.sideboard)  # sum of dicts
+        self.card_prices = card_prices                 # dict of prices in the right currency (€ or $)
+        self.list = None                               # overwritten by self.detail when url of specific deck is called
+        self.list_side = None
+        self.cards = sum(self.total.values())
+
+        # values changed by the loop
+        self.your_cards = 0  # cards you already have
+        self.price = 0
+        self.your_price = 0  # price you already have but will become price you need
+
+        # ONE LOOP TO CHANGE THEM ALL: only one loop might be more efficient than optimizing single operations
+        for card in self.total:
+            card_lower = card.lower()
+
+            # double cards are often not included in decklist or in prices_eur.
+            # Solve by calling card_prices of only first part e.g.: "status // statue" becomes "status"
+            try:
+                card_price = self.card_prices[card_lower]
+            except KeyError:
+                card_price = self.card_prices[card_lower.split(" // ")[0]]
+
+            self.price += self.total[card] * card_price
+
+            if self.arena is True and card not in BASIC_LANDS:  # basic land don't matter
+                # increase wc count for every card.
+                # self.wc[rarity[card]] += self.total[card]  # if lands missing, use next comment
+                # add to rarity: {'Forest': "Basic Land", 'Swamp': "Basic Land", 'Mountain': "Basic Land",
+                # 'Plains': "Basic Land", 'Island': "Basic Land"}
+                card_rarity = rarity.get(card, rarity[card.split(" // ")[0]])
+                self.wc[card_rarity] += self.total[card]
+
+            if card_lower in coll_dict:
+                minimum = min(self.total[card], coll_dict[card_lower])  # check if more copies than necessary
+                self.your_cards += minimum
+                self.your_price += minimum * card_price
+
+                if self.arena is True and card not in BASIC_LANDS:
+                    # reduce wc needed if you have card in coll_dict by min(cards needed, cards in coll_dict)
+                    self.wc[rarity[card]] -= minimum
+
+        self.price = int(self.price)  # solve Python weird numbers
+        self.cards_you_need = self.cards - self.your_cards
+        self.your_price = int(self.price - self.your_price)  # now self.your_price is price you need
+        self.value_you_own = int((1 - self.your_price / self.price) * 100)
+
+    def add_list(self, main_or_side: Dict[str, int]) -> list:
+        """
+        Method used by .detail() method to return list of dicts with info about every card in main_or_side (NOT
+        self.total, cuz cards in sideboard could be also in main and screw up).
+
+        :param main_or_side: self.mainboard or self.sideboard :example : class.detail() will set
+            self.list = self.add_list(self.mainboard)
+        :return :  e.g.: [
+                    {"name": "mox", "copies_owned": 1, "copies_total": 4, "copies_missing": 3, "price_for_you": 50,
+                            "tot_price": 200, "arena": "(DMN) 196", "wildcards": "3 Mythic" },
+                    {"name": "knight", "copies_owned": 2, "copies_total": 3, "copies_missing": 1, "price_for_you": 10,
+                            "tot_price": 30, "arena": "(XLN) 72", "wildcards": "1 Rare" },
+                    ...
+                    ]
+        """
+        info = list()
+        for card in main_or_side:
+            card_lower = card.lower()
+            temp = {'name': card, 'copies_total': main_or_side[card]}
+            # if copies > necessary, copies_owned = copies_total
+            temp['copies_owned'] = min(self.collection.get(card.lower(), 0), temp['copies_total'])
+            temp['copies_missing'] = temp['copies_total'] - temp['copies_owned']
+            temp['price_for_you'] = round(temp['copies_missing'] * self.card_prices[card_lower], 2)
+            temp['tot_price'] = round(temp['copies_total'] * self.card_prices[card_lower], 2)
+
+            if self.arena is True:
+                temp['wildcards'] = rarity.get(card, rarity[card.split(" // ")[0]])
+            info.append(temp)
+        return info
+
+    def detail(self):
+        """
+        Generate info about every card (used only when user clicks on single deck) by initializing self.list attribute.
+        """
+        self.list = self.add_list(self.mainboard)
+        if self.sideboard is not None:
+            self.list_side = self.add_list(self.sideboard)
+
+
+def get_format(stringa_or_dict: Union[str, Dict[str, Dict[str, int]]]) -> Union[str, Dict[str, Dict[str, int]]]:
+    """
+    Return format_name of format or format of format_name.
+
+    :param stringa_or_dict: string (e.g.: 'Modern') or dict (e.g.: Modern)
+    :return: opposite data type (dict or string)
+    -------
+    Impure Function
+    """
+    # dictionaries are not hashable, so only strings are in format_converter
+    format_converter = {'Standard': Standard, 'Brawl': Brawl, 'Historic': Historic, 'Pioneer': Pioneer,
+                        'Modern': Modern, 'Legacy': Legacy, 'Legacy Budget To': LegacyBudgetToTier, 'Pauper': Pauper,
+                        'Vintage': Vintage, 'Cube': Cube, 'Historic Brawl': Historic_Brawl,
+                        'Commander 1v1': Commander_1v1, 'Commander': Commander}
+
+    if isinstance(stringa_or_dict, str):
+        return format_converter[stringa_or_dict]
+
+    elif stringa_or_dict == Modern:
+        return "Modern"
+    elif stringa_or_dict == Legacy:
+        return "Legacy"
+    elif stringa_or_dict == Pauper:
+        return "Pauper"
+    elif stringa_or_dict == Standard:
+        return "Standard"
+    elif stringa_or_dict == LegacyBudgetToTier:
+        return "Legacy Budget To"
+    elif stringa_or_dict == Pioneer:
+        return 'Pioneer'
+    elif stringa_or_dict == Brawl:
+        return 'Brawl'
+    elif stringa_or_dict == Historic:
+        return 'Historic'
+    elif stringa_or_dict == Vintage:
+        return 'Vintage'
+    elif stringa_or_dict == Cube:
+        return 'Cube'
+    elif stringa_or_dict == Historic_Brawl:
+        return 'Historic Brawl'
+    elif stringa_or_dict == Commander_1v1:
+        return 'Commander 1v1'
+    elif stringa_or_dict == Commander:
+        return 'Commander'
+
+
+def get_db(my_collection: dict, format_dict: dict, card_prices: dict, sorted_by_value_you_have=True) -> \
+        List[Dict[str, Union[int, Any]]]:
+    """
+    Create instances of class Deck for every deck in format_dict, Return list of dictionaries with deck infos to
+    populate html tables.
+    """
+    db = []
+    for name, pack in format_dict.items():
+        nth_deck = Deck(name, pack, get_format(format_dict), format_dict, my_collection, card_prices)
+        # UNCOMMENT IN DEVELOPMENT to test all decklists (like if clicking on every link):
+        # nth_deck.detail()
+        temp = {'name': nth_deck.name, 'formato': nth_deck.format_name, 'value': nth_deck.value_you_own,
+                'tot_price': nth_deck.price, 'your_price': nth_deck.your_price, 'cards_needed': nth_deck.cards_you_need,
+                'cards_total': nth_deck.cards, 'wc': nth_deck.wc}
+
+        db.append(temp)
+
+    if sorted_by_value_you_have is True:
+        db = sorted(db, key=lambda x: x['value'], reverse=True)
+    return db
+
+
+def price_collection(price_dict: Dict[str, float], collec_dict: Dict[str, int]) -> Dict[str, Any]:
+    """
+    Evaluate collec_dict with prices from price_dict.
+    :price_dict: dict of prices
+    :collec_dict: dict to analyze
+    :return:           {
+                        "tot_copies": 6,
+                        "different_cards": 2,
+                        "tot_value": 320,
+                        "unrecognized": ["a", "b"],
+                        "recognized": [
+                                        {"name": "tarmogoyf", "copies": 2, "price": 50, "tot_price": 100},
+                                        {"name": "scalding tarn", "copies": 4, "price": 55, "tot_price": 220}
+                                    ]
+                        }
+    """
+    unrec_recognized = {}  # result. dict including list(unrecognized) and list(recognized) cards
+    unrec = []
+    recognized = []
+    temp = {}
+    tot_copies = 0
+    different_cards = 0
+    tot_value = 0
+    for card in collec_dict.keys():
+        if card in price_dict.keys():
+            tot_copies = tot_copies + collec_dict[card]
+            different_cards += 1
+            temp["name"] = card
+            temp["copies"] = collec_dict[card]
+            temp["price"] = price_dict[card]
+            temp["tot_price"] = round(temp["copies"] * temp["price"], 2)
+            recognized.append(temp)
+            tot_value = tot_value + temp["tot_price"]
+            temp = {}
+        else:
+            unrec.append(card)
+    unrec_recognized.update(
+        {"tot_copies": tot_copies,
+         "different_cards": different_cards,
+         "tot_value": round(tot_value, 2),
+         "recognized": recognized,
+         "unrecognized": unrec}
+    )
+    if len(recognized) > 0:
+        unrec_recognized["recognized"] = recognized
+    return unrec_recognized
+
+
+if __name__ == "__main__":  # test
+    from prices_eur import prices_eur
+
+    collection = {"ancient stirrings": 4, "kroxa, titan of death's hunger": 4}
+    test = {"disdainful stroke": 3}
+
+    deck = list(Standard.keys())[0]
+    formato = get_db(collection, get_format("Standard"), prices_eur)
+    # dk = Deck('Hazoret Red', Historic_Brawl['Hazoret Red'], 'Historic_Brawl', Historic_Brawl, {}, prices_eur)
+    import pdb
+    pdb.set_trace()
---
+++ Best Mtg Deck/start.py
@@ -0,0 +1,199 @@
+"""
+Urls and start of the website.
+"""
+import json
+import os
+import random
+import sys
+
+from flask import Flask, render_template, request, send_file, session
+
+from bestdeck import Deck, get_db, get_format, price_collection
+from mtg_parser import BASIC_LANDS, clean_input, parse_collection
+from prices_eur import prices_eur
+from prices_usd import prices_usd
+
+
+def get_currency(wk_local_proxy) -> (str, dict):
+    """
+    Get tuple of str for html page ($ or €) and dict of prices.
+    :param wk_local_proxy: class 'werkzeug.local.LocalProxy'
+    :return: tuple (str, dict)
+    """
+    if "dollars" not in wk_local_proxy.form:  # if tick box for USD is not checked by user
+        currency_html = "&euro;"  # € sign for html
+        card_prices = prices_eur
+    else:
+        currency_html = "&#36;"  # $ sign for html
+        card_prices = prices_usd
+    return currency_html, card_prices
+
+
+app = Flask(__name__)  # initialize website
+
+# cryptography for cookie used as key of stored collections dict
+app.secret_key = os.getenv("SECRET KEY", b':\xafq\x87\xe0\x12\xbfU\xeeC\x9b\x17\xcfs\xaf)')
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/sign")
+def sign():
+    """
+    Form to insert coll_dict.
+    """
+    return render_template("sign.html")
+
+
+@app.route("/<format_name>/<currency>", methods=["POST", "GET"])
+def show_single_format(format_name, currency="€"):
+    try:
+        with open("collections.json") as data:
+            collections = json.load(data)
+
+    except Exception:  # if collections.json is too big, create a new one
+        with open("collections.json", "w") as data:
+            json.dump({"a": 1}, data)
+        collections = dict()
+
+    if request.method == "POST":
+        # save collection
+        try:
+            # comment is name of input box where user inserts collection
+            collection = parse_collection(request.form["comment"])
+            session["user_code"] = str(random.random())
+            collections[session["user_code"]] = collection
+            with open("collections.json", "w") as j:
+                json.dump(collections, j)
+        except (IndexError, ValueError) as err:
+            mistake = str(err.args[0])
+            return render_template("wrongformat.html", error=mistake)
+
+    else:  # elif request.method == "GET":
+        # retrieve collection
+        if 'user_code' in session:
+            try:
+                collection = collections[session["user_code"]]
+            except KeyError:
+                print(f"Error on url /calc/{format_name}/{currency}: Lost collection of {session['user_code']}",
+                      file=sys.stderr)
+                return render_template("lostcollection.html")
+        else:
+            collection = BASIC_LANDS
+
+    if currency == "€" or currency == "$":
+        card_prices = {"€": prices_eur, "$": prices_usd}[currency]
+        return render_template("format.html",
+                               format_name=format_name,
+                               formato=get_db(collection, get_format(format_name), card_prices),
+                               currency=currency,
+                               prices=card_prices)
+    elif currency == "mtga":
+        return render_template("mtga_formats.html",
+                               format_name=format_name,
+                               formato=get_db(collection, get_format(format_name), prices_eur),
+                               currency=currency,
+                               prices=prices_eur)
+    else:
+        return render_template("wrongformat.html", error="The url you inserted")
+
+
+@app.route("/calc/<format_name>/<deck_name>/<currency>", strict_slashes=False, methods=["GET"])
+def calc(format_name, deck_name, currency):
+    """
+    Page showing all cards of a particular deck.
+    """
+    try:
+        with open("collections.json") as data:
+            collections = json.load(data)
+    except:  # if collections.json is too big, create a new one
+        with open("collections.json", "w") as data:
+            json.dump({"a": 1}, data)
+        collections = dict()
+
+    prices = prices_usd if currency == "$" else prices_eur
+
+    if "user_code" in session:
+        try:
+            collection = collections[session["user_code"]]
+        except KeyError:
+            print(f"Error on url /calc/{format_name}/{currency}: Lost collection of {session['user_code']}",
+                  file=sys.stderr)
+            return render_template("lostcollection.html")
+    else:
+        collection = BASIC_LANDS
+
+    try:
+        dk = Deck(deck_name, get_format(format_name)[deck_name], format_name, get_format(format_name), collection,
+                  prices)
+        dk.detail()  # add info for each card
+
+        return render_template("deck.html", deck=dk, title=deck_name, currency=currency)
+
+    except KeyError:
+        # if user changes deck_name in url / cannot find deck
+        # print(f"Error on url /calc/{format_name}/{deck_name}/{currency}: Missing Deck", file=sys.stderr)
+        return render_template("wrongformat.html",
+                               error=f"{format_name} does not contain {deck_name} or does not contain {deck_name} "
+                                     f"anymore. The requested URL was not found on the server.")
+    except TypeError:
+        # if user changes format_name in url / cannot find format_name (Type Error is raised by get_formato() )
+        print(f"Error on url /calc/{format_name}/{deck_name}/{currency}: {format_name} not found", file=sys.stderr)
+        return render_template("wrongformat.html",
+                               error=f"{format_name} does not exist. The requested URL was not found on the server.")
+
+
+@app.route("/value")
+def values():
+    return render_template("value.html")
+
+
+@app.route("/value_result", methods=["POST"])
+def evaluate():
+    try:
+        collection = parse_collection(request.form["comment"], add_basics=False)
+    except (IndexError, ValueError) as err:
+        # mistake = str(err.args[0][0])  # err.args = (["aaa"])  err.args[0] = ["aaa"]
+        mistake = str(err).replace("\n", "<br>")
+        return render_template("wrongformat.html", error=mistake)
+    currency_html, prices = get_currency(request)
+
+    return render_template("your_value.html", value=price_collection(prices, collection), currency=currency_html)
+
+
+@app.route("/decklist_formatter", methods=["POST", "GET"])
+def page_decklist_formatter():
+    if request.method == "GET":
+        return render_template("decklist_formatter.html",
+                               previous_user_info="",
+                               parsed_output="")
+    from format_deck import deck_formatter
+    bbcode_deck, html_deck = deck_formatter(
+        cards=clean_input(request.form["comment"]),
+        deck_name=request.form['info_deck_name'],
+        player_name=request.form['info_player_name'],
+        event_name=request.form['info_event_name'],
+        role=request.form["info_player_role"],
+        note=request.form['info_note_redazione'])
+    return render_template("decklist_formatter.html",
+                           previous_user_input=request.form["comment"],
+                           parsed_output=bbcode_deck,
+                           test=html_deck)
+
+
+@app.route("/download1.2")
+def download_file():
+    return send_file("outputs/OrensMTGA-EasyExporterV1.2.exe", mimetype="exe",
+                     attachment_filename="OrensMTGA-EasyExporterV1.2.exe", as_attachment=True)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("wrongformat.html", error="The requested URL was not found on the server and")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
